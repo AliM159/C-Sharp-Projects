23cd58b4582bebc83a46013146a6db97625523bb
Ali Mustafa <167823529+AliM159@users.noreply.github.com>
2025-11-23 15:23:38 -0500

    Add files via upload

---
+++ Car/CarClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarObjectModel
+{
+    internal class CarClass
+    {
+        private string _modelMake;
+        private int _year, _horsePower;
+        private bool _isDrivable;
+        private double _price;
+
+
+        public CarClass (  string modelMake, int year, int horsePower,  double price , bool isDrivable = true)
+        {
+            this._year = year;
+            this._horsePower = horsePower;
+            this._modelMake = modelMake;
+            this._isDrivable = isDrivable;
+            this._price = price;  
+
+            
+        }
+
+
+        public override string ToString()
+        {
+            return $"Model: {_modelMake} \nYear: {_year} \nHorsepower: {_horsePower} \nDrivable: {_isDrivable} \nPrice: {_price :C} \n";
+        }
+    }
+}
---
+++ Car/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CarObjectModel;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CarClass car1 = new CarClass("Nissan Skyline R34", 2021, 280, 86000);
+        CarClass car2 = new CarClass("Hyundai Elantra", 2025, 147, 31200);
+        CarClass car3 = new CarClass("Lamborghini Avantador SVJ", 2023, 770, 700300);
+        CarClass car4 = new CarClass("Toyota Hilux", 2013, 158, 11000, false);
+        CarClass car5 = new CarClass("Tesla Model X", 2022, 670, 124000);
+
+
+        Console.WriteLine(car1.ToString());
+        Console.WriteLine(car2.ToString());
+        Console.WriteLine(car3.ToString());
+        Console.WriteLine(car4.ToString());
+        Console.WriteLine(car5.ToString());
+
+        
+
+    }
+}
